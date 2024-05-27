3f886ac5539bcc688c357e0b23aedce63ef3ac9f
Yuri Assine <160561603+srBolseiro@users.noreply.github.com>
2024-05-27 00:32:10 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,68 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, url_for, render_template, request, session
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
+from helpers import login_required, validate_credential
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config.from_object(__name__)
+Session(app)
+
+# DATABASE LINK
+db = SQL("sqlite:///database.db")
+
+# FUNCTIONS!!
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///shire.db")
+
+# INDEX
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+#  REGISTER
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        render_template("register.html")
+    else:
+        return "post register"
+
+# LOGIN
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        render_template("login.html")
+    else:
+        return "post login"
+
+# LOGOUT
+@app.route("/logout/")
+@login_required
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+
+
+
+
+# RENDER_SHEET
+@app.route("/player/<sheet_id>/", methods=["GET"])
+@login_required
+def render_sheet(sheet_id):
+    return sheet_id
