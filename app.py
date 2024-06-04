61ef11bfb4818f414bad151a9f069408ff2a75a3
bolsa <160561603+srBolseiro@users.noreply.github.com>
2024-06-04 02:58:05 -0300

    index ok

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from cs50 import SQL
 from flask import Flask, flash, redirect, url_for, render_template, request, session
 from flask_session import Session
-from werkzeug.security import generate_password_hash, check_password_hash
 from helpers import login_required, validate_credential
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Configure application
 app = Flask(__name__)
@@ -13,10 +13,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config.from_object(__name__)
 Session(app)
 
-# DATABASE LINK
+# Database link
 db = SQL("sqlite:///shire.db")
 
 # INDEX
 @app.route("/")
 def index():
-    return render_template("index.html")
+        return render_template("index.html")
